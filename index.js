3a396716ba12b52ed53d6c5bf3aa5a92d28fe187
Yuri <yuridacostagouveia@gmail.com>
2020-04-21 15:54:58 -0300

    Implementation: refactoring

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 var express = require('express');
 var socket = require('socket.io');
 
-//Config da aplicação
+// App setup
 var app = express();
-var server = app.listen(8080, function(){
-	console.log('servidor rodando na porta 8080');
-});//Porta a qual o servidor irá escutar
-
-//static files
-app.use(express.static('public'));
-
-//socket setup
-var io = socket(server);
-
-io.on('connection', function(socket){
-	console.log('conexao do socket criada', socket.id);
-
-	socket.on('chat', function(data){
-		io.sockets.emit('chat', data);
-	});
-
-	socket.on('typing', function(data){
-		socket.broadcast.emit('typing', data); //ira emitir quem esta digitando para todos os clientes, exceto o proprio
-	});	
+var server = app.listen(4000, function(){
+    console.log('listening for requests on port 4000,');
 });
 
+// Static files
+app.use(express.static('public'));
 
+// Socket setup & pass server
+var io = socket(server);
+io.on('connection', (socket) => {
+
+    console.log('made socket connection', socket.id);
+
+    // Handle chat event
+    socket.on('chat', function(data){
+        // console.log(data);
+        io.sockets.emit('chat', data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+
+});
